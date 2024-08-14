3a8fc6e26c60239f351d257783c6e4155405b9ed
dkdinesh7399 <dkdinesh7398@gmail.com>
2024-08-14 20:36:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import SliderContent from "./component1.js/SliderContent";
+//import Dots from "./component1.js/Dots";
+//import Arrow from "./component1.js/Arrow";
+//import SliderImage from "./component1.js/SliderImage";
+//import "./App.css";
+import Slider from "./component1/Slider";
+
+
+
+function App(){
+   
+    
+        return(
+          <div>
+          <Slider/>
+          </div>
+        )
+    }
+
+    export default App();
